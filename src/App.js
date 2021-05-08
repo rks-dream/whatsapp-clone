c42a0829c6d8adcf5462367cad82e90d4decc0c3
Your Name <you@example.com>
2021-05-08 08:17:35 +0530

    2nd version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import React from 'react';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello MERN stack developer world</h1>
+    <div className="app">
+      <div className="app__body">
+         <Sidebar />
+         <Chat />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAsc6wYTv9OdQEQ4HlXbqnP2J902YDnZu8",
+    authDomain: "whatsapp-mern-bff0e.firebaseapp.com",
+    projectId: "whatsapp-mern-bff0e",
+    storageBucket: "whatsapp-mern-bff0e.appspot.com",
+    messagingSenderId: "583613325813",
+    appId: "1:583613325813:web:3030a0cfb292925b1740bc"
+  };
