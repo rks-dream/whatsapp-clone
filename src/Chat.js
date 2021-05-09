4577a6d76ab79a8483a0dc2fa15e3f659e106d14
Your Name <you@example.com>
2021-05-09 15:46:12 +0530

    Front-end

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,65 @@
 import React from 'react';
 import './Chat.css';
+import {IconButton,Avatar} from '@material-ui/core';
+// import AttachFileIcon from '@material-ui/icons/AttachFile';
+// import SearchIcon from '@material-ui/icons/Search';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { AttachFile, SearchOutlined } from '@material-ui/icons';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>I am chat component</h1>
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Family</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Sakina</span>
+                    Hum aapse beintehaan mohabbat krte h
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat__message">
+                    <span className="chat__name">Mummy</span>
+                    Sakina meri bahu bnegi
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat__message  chat__receiver">
+                    <span className="chat__name">Rahul</span>
+                    Ha, mummy..jarur
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input placeholder="Type a message" type="text" />
+                    <button type="submit">Send a message</button>
+                </form>
+                <MicIcon />
+            </div>
         </div>
     )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import './Sidebar.css';
+import {IconButton,Avatar} from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+// import SearchOutlined from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+import Sidebarchat from './SidebarChat';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>I am a sidebar</h1>
+            
+            <div className="sidebar__header">
+                <Avatar src="https://avatars.githubusercontent.com/u/61194112?s=400&u=1129cc0af63e3a24e3d6b96d26d5bfbf8ce33bf4&v=4"/>
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchIcon />
+                    <input type="text" placeholder="search or start new chat" />
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <Sidebarchat />
+                <Sidebarchat />
+                <Sidebarchat />
+                <Sidebarchat />
+                <Sidebarchat />
+            </div>
         </div>
-    )
+    );
 }
 
 export default Sidebar
